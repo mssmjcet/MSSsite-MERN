369ca39036155b50acd1d264d81e1e23421da09e
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2022-11-04 23:28:00 +0530

    admin dashboard frontend created

--- client/src/components/admin/Navbar.js
+++ client/src/components/admin/Navbar.js
@@ -1,16 +1,15 @@
 const Navbar=()=>{
     return(
         <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">MSS MJCET</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <a class="nav-item nav-link" href="/admin/home">Home </a>
+      <a class="nav-item nav-link active" href="#">Registrations<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="/admin/eventsDashboard">Events</a>
     </div>
   </div>
 </nav>
