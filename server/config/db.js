232f62e50396ed990b9b84e5bc8a174a4781cfaa
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2022-11-11 21:27:11 +0530

    project files removed to change project structure

--- server/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-
-const connectDB=mongoose.connect("mongodb+srv://SyedMohammedQuadri:Ab13579@cluster0.nhva0zg.mongodb.net/MsterDB");
-
-
-module.exports = connectDB;
--- server/routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const {getRegistrationsWithEventId}=require("../controllers/adminController");
-
-const router=express.Router();
-router.route('/getRegistrationInfo').post(getRegistrationsWithEventId);
-
-module.exports = router;
